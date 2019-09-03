b7db0709effec83581c3f12263303b32beec198c
hardway2008 <15618747689@163.com>
2019-09-04 01:13:20 +0800

    Add working environment

---
+++ Makefile
@@ -0,0 +1,4 @@
+x86:
+	cd scripts; ./build_x86.sh;
+clean:
+	rm -rf bin/ build_x86/
---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+### 
+# @Date: 2019-09-04 01:38:09
+ # @LastEditors: jlwang
+ # @LastEditTime: 2019-09-04 01:38:48
+ ###
+../bin/running_raspberry
